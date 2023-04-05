5b65350426c3c0d15d50b190ca32b939108e59ca
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2023-04-05 17:37:23 +0530

    Add trips page

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -1,16 +1,20 @@
 'use client';
+
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-
 const Logo = () => {
-    const Router = useRouter();
+    const router = useRouter();
+
     return (
-        <Image alt="Logo"
+        <Image
+            onClick={() => router.push('/')}
             className="hidden md:block cursor-pointer"
+            src="/images/logo.png"
             height="100"
             width="100"
-            src="/images/logo.png"></Image>
+            alt="Logo"
+        />
     );
 }
 
