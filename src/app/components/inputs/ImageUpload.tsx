25fc9777e4c04cdb1dee19dd029b02dbfe95e045
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2023-04-05 07:16:34 +0530

    Add listing page

--- src/app/components/inputs/ImageUpload.tsx
+++ src/app/components/inputs/ImageUpload.tsx
@@ -9,7 +9,7 @@ declare global {
     var cloudinary: any
 }
 
-const uploadPreset = "pgc9ehd5";
+const uploadPreset = "nlvuxwdh";
 
 interface ImageUploadProps {
     onChange: (value: string) => void;
